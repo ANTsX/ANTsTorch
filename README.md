a29b63318639dcd1c4af26b7139eb2c21eb0611e
Nick Tustison <ntustison@gmail.com>
2023-06-22 14:47:35 -0400

    ENH:  Set-up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DeepSiMLR
+
+__Interpretable, similarity-driven multi-view embeddings from high-dimensional biomedical data__
+
+## Bibliography
+
+* (Avants et al., Similarity-driven multi-view embeddings from high-dimensional biomedical data.  Nat Comput Sci.  (2):143-152, Feb 2021)[https://pubmed.ncbi.nlm.nih.gov/33796865/]
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+long_description = open("README.md").read()
+
+setup(name='deepsimlr',
+      version='0.0.0',
+      description='Interpretable, similarity-driven multi-view embeddings from high-dimensional biomedical data.',
+      long_description=long_description,
+      long_description_content_type="text/markdown; charset=UTF-8; variant=GFM",
+      url='https://github.com/ntustison/DeepSiMLR',
+      author='Brian B. Avants and Nicholas J. Tustison',
+      author_email='ntustison@gmail.com',
+      packages=['deepsimlr','deepsimlr/architectures','deepsimlr/utilities'],
+      install_requires=['antspyx','torch','scikit-learn','numpy','requests','statsmodels','matplotlib'],
+      zip_safe=False)
