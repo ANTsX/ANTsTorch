82043a3a1a8ca0305ab9a8bbd6536ca6a577bc89
stnava <stnava@gmail.com>
2023-09-14 06:20:09 -0400

    WIP: ...

--- tests/ukbb_prep.R
+++ tests/ukbb_prep.R
@@ -14,27 +14,27 @@ if ( ! exists("dd") ) {
         }
     dd$Years.bl=dd$age_MRI-dd$subjectAge_BL
     dd$DX='CN'
-}
-subct=table( dd$eid )
-crosssubs=names(subct[subct==1])
-longsubs=names(subct[subct==2])
-print(paste("Cross:",length(crosssubs),"long:",length(longsubs)))
-t1names=c( 
-    getNamesFromDataframe( "T1Hier_vol", dd, 
-        exclusions=c("_BL","_delta","evratio","Grade","mhdist","RandB","outli","templateL1") ),
-    getNamesFromDataframe( "T1Hier_thk", dd, 
-        exclusions=c("_BL","_delta","evratio","Grade","mhdist","RandB","outli","templateL1") ) )
-dtinames=getNamesFromDataframe( "DTI"  , dd, exclusions=c("_BL","_delta","cnx","FD","motion","SNR","evr","ssnr","dvars","tsnr","volume") )
-rsfnames=getNamesFromDataframe( "rsfMRI"  , dd, exclusions=c("_BL","_delta","cnx","FD","motion","SNR","evr","ssnr","dvars","tsnr","volume","_alff") )
+    subct=table( dd$eid )
+    crosssubs=names(subct[subct==1])
+    longsubs=names(subct[subct==2])
+    print(paste("Cross:",length(crosssubs),"long:",length(longsubs)))
+    t1names=c( 
+        getNamesFromDataframe( "T1Hier_vol", dd, 
+            exclusions=c("_BL","_delta","evratio","Grade","mhdist","RandB","outli","templateL1") ),
+        getNamesFromDataframe( "T1Hier_thk", dd, 
+            exclusions=c("_BL","_delta","evratio","Grade","mhdist","RandB","outli","templateL1") ) )
+    dtinames=getNamesFromDataframe( "DTI"  , dd, exclusions=c("_BL","_delta","cnx","FD","motion","SNR","evr","ssnr","dvars","tsnr","volume") )
+    rsfnames=getNamesFromDataframe( "rsfMRI"  , dd, exclusions=c("_BL","_delta","cnx","FD","motion","SNR","evr","ssnr","dvars","tsnr","volume","_alff") )
 
-#### cross-sectional
-rsfdd = antsrimpute(scale( dd[,rsfnames] ))
-dtidd = antsrimpute(scale( dd[,dtinames] ))
-t1dd = antsrimpute(scale( dd[,t1names] ))
-xsel = dd$eid %in% crosssubs
-write.csv( rsfdd[xsel,], "/tmp/ukrsfx.csv", row.names=FALSE)
-write.csv( dtidd[xsel,], "/tmp/ukdtix.csv", row.names=FALSE)
-write.csv( t1dd[xsel,], "/tmp/ukt1x.csv", row.names=FALSE)
+    #### cross-sectional
+    rsfdd = antsrimpute(scale( dd[,rsfnames] ))
+    dtidd = antsrimpute(scale( dd[,dtinames] ))
+    t1dd = antsrimpute(scale( dd[,t1names] ))
+    xsel = dd$eid %in% crosssubs
+    write.csv( rsfdd[xsel,], "/tmp/ukrsfx.csv", row.names=FALSE)
+    write.csv( dtidd[xsel,], "/tmp/ukdtix.csv", row.names=FALSE)
+    write.csv( t1dd[xsel,], "/tmp/ukt1x.csv", row.names=FALSE)
+}
 #### longitudinal
 lsel = dd$eid %in% longsubs
 write.csv( rsfdd[lsel,], "/tmp/ukrsfl.csv", row.names=FALSE)
@@ -56,13 +56,14 @@ colnames(prsf)=paste0("rsf",1:ncol(pt1))
 
 library(lmerTest)
 ee=cbind(dd[lsel,],pt1,pdti,prsf)
+nsim=ncol(pt1)
 simnames=paste(
-    paste0(paste0("t1",1:ncol(pt1)),collapse='+'), "+",
-    paste0(paste0("dti",1:ncol(pt1)),collapse='+'),"+",
-    paste0(paste0("rsf",1:ncol(pt1)),collapse='+')
+    paste0(paste0("t1",1:nsim),collapse='+'), "+",
+    paste0(paste0("dti",1:nsim),collapse='+'),"+",
+    paste0(paste0("rsf",1:nsim),collapse='+')
 )
 mf=paste("age_MRI~(1|eid)+sex_f31_0_0+",simnames)
-summary(lmer(mf,data=ee))
+print(summary(lmer(mf,data=ee)))
 
 
 intnames=getNamesFromDataframe("luid",dd)
@@ -72,24 +73,27 @@ for ( i in intnames) {
 }
 
 mf=paste("fluid_intelligence_score_f20016~(1|eid)+age_MRI+sex_f31_0_0+",simnames)
-summary(lmer(mf,data=ee))
-
+print(summary(lmer(mf,data=ee)))
 
 
 # some clustering
 #
 pt1=data.frame(data.matrix(t1dd[xsel,]) %*% t(data.matrix(ev0)))
-colnames(pt1)=paste0("t1",1:ncol(pt1))
+colnames(pt1)=paste0("t1",1:nsim)
 pdti=data.frame(data.matrix(dtidd[xsel,]) %*% t(data.matrix(ev1)))
-colnames(pdti)=paste0("dti",1:ncol(pt1))
+colnames(pdti)=paste0("dti",1:nsim)
 prsf=data.frame(data.matrix(rsfdd[xsel,]) %*% t(data.matrix(ev2)))
-colnames(prsf)=paste0("rsf",1:ncol(pt1))
-ee=cbind(dd[xsel,],pt1,pdti,prsf)
+colnames(prsf)=paste0("rsf",1:nsim)
+nsimu=3
+extras=cbind(pt1[,1:nsimu],pdti[,1:nsimu],prsf[,1:nsimu])
+ee=cbind(dd[xsel,],extras)
+nclust = 4
+#
 myclust = trainSubtypeClusterMulti(
        ee,
-       measureColumns=c(colnames(pt1),colnames(pdti),colnames(prsf)),
-       method = "kmeans",
-       6  )
+       measureColumns=colnames(extras),
+       method = "kmeansflex",
+       nclust  )
 
 
 pt1l=data.frame(data.matrix(t1dd[lsel,]) %*% t(data.matrix(ev0)))
@@ -102,12 +106,35 @@ ff=cbind(dd[lsel,],pt1l,pdtil,prsfl)
 
 fff = predictSubtypeClusterMulti(
        ff,
-       measureColumns=c(colnames(pt1),colnames(pdti),colnames(prsf)),
+       measureColumns=colnames(extras),
        myclust,
        clustername = "KMC",
-       'eid' )
+       'eid', 'Years.bl', 0 )
      
 #
-fff$age_MRI_round = round( fff$Years.bl)
-plotSubtypeChange( fff, idvar='eid', measurement='age_MRI', 
-    subtype='KMC', vizname='age_MRI_round' )
+
+pp=getNamesFromDataframe( "", fff, exclusions=c("T1Hier","rsfMRI","DTI","mean_fa","mean_mo","mean_l2","T1w","mean_l3","mean_l1") )
+
+isbl = fff$Years.bl==0
+for ( x in sample(pp) ) {
+    if ( class(fff[,x]) == 'numeric' ) {
+        zzz=fff[!is.na(fff[,x]),]
+        usesubs=intersect( zzz$eid[isbl], zzz$eid[ !isbl ])
+        if ( length(usesubs)  > 1000 ) {
+            # check for change
+            ischange=FALSE
+            for ( k in 1:100 )
+                ischange = ischange | var( zzz[ zzz$eid == usesubs[k], x ] > 0 )
+            if ( ischange ) {
+                zzz=zzz[zzz$eid %in% usesubs, ]
+                print(x)
+                zzz$yblr = round( zzz$Years.bl*2)
+                tsel = table( zzz$yblr )
+                tsel = names(tsel[tsel > 50])
+                plotSubtypeChange( zzz[zzz$yblr %in% tsel,], idvar='eid',
+                    measurement=x, 
+                    subtype='KMC', vizname='yblr' ) %>% print() 
+            }
+        }
+    }
+}
