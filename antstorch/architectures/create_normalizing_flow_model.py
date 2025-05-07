9950db426a94ec532f90d1c2e16a357653911abd
ntustison <ntustison@gmail.com>
2025-05-07 11:48:28 -0700

    ENH:  Brian's rec. of initial dense layers.

--- antstorch/architectures/create_normalizing_flow_model.py
+++ antstorch/architectures/create_normalizing_flow_model.py
@@ -5,8 +5,7 @@ import normflows as nf
 def create_real_nvp_normalizing_flow_model(latent_size,
                                            K=64,
                                            q0=None,
-                                           number_of_initial_dense_layers=0,
-                                           initial_leaky_relu=0.0):
+                                           number_of_initial_dense_layers=0):
     """
     Create Real NVP model.
 
@@ -23,6 +22,9 @@ def create_real_nvp_normalizing_flow_model(latent_size,
         e.g., diagonal gaussian q0 = nf. DiagGaussian(latent_size).
         None is also a possibility. 
 
+    number_of_initial_dense_layers : integer
+        Number of initial dense layers.    
+
     Returns
     -------
     PyTorch model
@@ -30,18 +32,21 @@ def create_real_nvp_normalizing_flow_model(latent_size,
 
     Example
     -------
-    >>> model = antstorch.create_real_nvp_normalizing_flow_model(512)
-    >>> torchinfo.summary(model)
+    >>> import normflows as nf
+    >>> input_size = 512
+    >>> q0 = nf.distributions.GaussianPCA(input_size, latent_dim=4)
+    >>> model = antstorch.create_real_nvp_normalizing_flow_model(input_size,
+                                                                 K=4, 
+                                                                 q0=q0, 
+                                                                 number_of_initial_dense_layers=2)
     """
 
     flows = []
 
-    for _ in range(number_of_initial_dense_layers):
-        flows.append(torch.nn.Sequential(
-                     torch.nn.Linear(in_features=latent_size,
-                                     out_features=latent_size),
-                     torch.nn.LeakyReLU(initial_leaky_relu)))
-
+    for _ in range(number_of_initial_dense_layers): 
+        d = nf.nets.MLP([latent_size, latent_size], output_fn="relu", init_zeros=False)
+        flows += [nf.flows.AffineCoupling(d, scale=False)]
+        
     b = torch.Tensor([1 if i % 2 == 0 else 0 for i in range(latent_size)])
     for i in range(K):
         s = nf.nets.MLP([latent_size, 2 * latent_size, latent_size], init_zeros=True)
