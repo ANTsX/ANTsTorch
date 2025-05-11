2f6c29ef6d76f7848a8036365a21b089a248b15a
Nick Tustison <ntustison@gmail.com>
2025-05-11 14:55:02 -0700

    ENH:  clean-up plotting portion of nhanes test.:

--- tests/nhanes/Scripts/plot_nhanes.py
+++ tests/nhanes/Scripts/plot_nhanes.py
@@ -5,7 +5,7 @@ import random
 import numpy as np
 import umap
 
-from torch.utils.data import Dataset
+import antstorch
 
 from matplotlib import pyplot as plt
 
@@ -22,57 +22,7 @@ pca_latent_dim = 4
 
 # Set up datasets/dataloaders
 
-class CsvDataset(Dataset):
-    def __init__(self,
-                 csv_file,
-                 alpha=0.01,
-                 do_normalize=True,
-                 do_data_augmentation=True,
-                 number_of_samples=1):
-        self.csv_file = csv_file
-        self.number_of_samples = number_of_samples
-        self.alpha = alpha
-        self.do_normalize = do_normalize
-        self.do_data_augmentation = do_data_augmentation
-        csv = pd.read_csv(csv_file)
-        self.csv_data_colnames = csv.columns  
-        self.csv_data = csv.to_numpy()
-        self.number_of_measurements = self.csv_data.shape[0]
-        self.csv_std = np.std(self.csv_data, axis=0)
-        self.csv_mean = np.mean(self.csv_data, axis=0)
-        self.csv_min = np.min(self.csv_data, axis=0)
-        self.csv_max = np.max(self.csv_data, axis=0)
-    def __len__(self):
-        return self.number_of_samples
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        random_index = random.sample(range(self.number_of_measurements), k=1)[0]
-        random_measurement = self.csv_data[random_index,:]
-        if self.do_data_augmentation:
-            random_measurement += np.random.normal(np.zeros(random_measurement.shape),
-                                                   self.alpha * self.csv_std)
-        if self.do_normalize:
-            random_measurement = self.normalize_data(random_measurement)
-        return random_measurement 
-    def normalize_data(self, data):
-        if len(data.shape) == 2:
-            min = np.tile(self.csv_min, (self.number_of_measurements, 1))
-            max = np.tile(self.csv_max, (self.number_of_measurements, 1))
-            normalized_data = (data - min) / (max - min)
-        else:
-            normalized_data = (data - self.csv_min) / (self.csv_max - self.csv_min) 
-        return normalized_data       
-    def denormalize_data(self, data):
-        if len(data.shape) == 2:
-            min = np.tile(self.csv_min, (self.number_of_measurements, 1))
-            max = np.tile(self.csv_max, (self.number_of_measurements, 1))
-            denormalized_data = data * (max - min) + min
-        else:
-            denormalized_data = data * (self.csv_max - self.csv_min) + self.csv_min
-        return denormalized_data       
-
-dataset = CsvDataset(csv_file=csv_file, number_of_samples=1000000)
+dataset = antstorch.CsvDataset(csv_file=csv_file, number_of_samples=1000000)
 number_of_columns = dataset.csv_data.shape[1]
 
 # Define model
@@ -91,10 +41,13 @@ for i in range(K):
         flows += [nf.flows.MaskedAffineFlow(1 - b, t, s)]
     flows += [nf.flows.ActNorm(latent_size)]
 
-# Construct flow model
-# q0 = nf.distributions.DiagGaussian(latent_size)
-q0 = nf.distributions.GaussianPCA(latent_size, latent_dim=pca_latent_dim)
-model = nf.NormalizingFlow(q0=q0, flows=flows)
+# Define model
+K = 64
+torch.manual_seed(0)
+
+q0 = nf.distributions.GaussianPCA(number_of_columns, latent_dim=pca_latent_dim)
+model = antstorch.create_real_nvp_normalizing_flow_model(number_of_columns,
+                                                         q0=q0)
 
 # Move model on GPU if available
 enable_cuda = True
