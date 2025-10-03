857c171a14e8a2a0e8231e9af074c3832a62d9da
ntustison <ntustison@gmail.com>
2025-10-03 06:58:02 -0700

    ENH:  Add cortical thickness.

--- antstorch/utilities/__init__.py
+++ antstorch/utilities/__init__.py
@@ -4,6 +4,8 @@ from .get_antstorch_data import get_antstorch_data
 from .brain_extraction import brain_extraction
 from .preprocess_image import preprocess_brain_image
 from .deep_atropos import deep_atropos
+from .cortical_thickness import cortical_thickness
+from .cortical_thickness import longitudinal_cortical_thickness
 
 from .mri_modality_classification import mri_modality_classification
 
