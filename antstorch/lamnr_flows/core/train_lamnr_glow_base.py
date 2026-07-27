9cb9211ff06a92e964e55a66682dad3d8ff99f65
ntustison <ntustison@gmail.com>
2026-07-27 08:19:25 -0700

    BUG:

--- antstorch/lamnr_flows/core/train_lamnr_glow_base.py
+++ antstorch/lamnr_flows/core/train_lamnr_glow_base.py
@@ -1401,8 +1401,8 @@ class BaseLAMNrTrainer(abc.ABC):
                 
                 grid = tv.utils.make_grid(imgs, nrow=10, padding=2, normalize=False)
                 tv.utils.save_image(grid, str(run_dir / f"input_data_view{vi}.png"))
-                
-                del x_all, x_v, imgs
+
+                del x_all, list_of_views, imgs
             
             del all_batches
             gc.collect()
